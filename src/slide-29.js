9f4905db8efaa2d53120c788a2e977039cfbf7da
Arne Martin Aurlien <am@aurlien.net>
2017-06-01 09:19:20 +0200

    Use array.prototype.fill

--- src/slide-29.js
+++ src/slide-29.js
@@ -3,9 +3,9 @@ function generator(ast) {
   function walk(part) {
     switch (part.type) {
     case 'diceExpression':
-      return [...Array(parseInt(part.lhs.value, 10))].map(_ => {
-        return '(Math.floor(Math.random() * ' + part.rhs.value + ')) + 1';
-      }).join(' + ');
+      return Array(parseInt(part.lhs.value, 10))
+        .fill('Math.floor(Math.random() * ' + part.rhs.value + ') + 1')
+        .join(' + ');
       break;
 
     case 'operatorExpression':
